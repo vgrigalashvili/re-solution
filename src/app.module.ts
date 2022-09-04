a5e1b0c9ef8ce2b51aa65b7e6408bc3c64dee9c3
vgrigalashvili <lado.grigalashvili@gmail.com>
2022-09-04 19:10:22 +0400

    Added:  User entity and repository.

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,12 +4,14 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
 
+import { User } from './users/user.entity';
+
 @Module({
   imports: [
     TypeOrmModule.forRoot({
       type: 'sqlite',
       database: 'db.sqlite',
-      entities: [],
+      entities: [User],
       synchronize: true,
     }),
     UsersModule,
