5e0939fc4f88797ca1bf7eda2ca72332247af75a
vgrigalashvili <lado.grigalashvili@gmail.com>
2022-09-06 03:23:34 +0400

    Added: user controller & service - (must refactor)

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,10 +1,76 @@
-import { Body, Controller, Post } from '@nestjs/common';
-import { CreateUserDto } from './dto';
+/*
+ *
+ * Users Controller.
+ *
+ */
+
+// Dependencies.
+import {
+  Body,
+  Controller,
+  Post,
+  Get,
+  Patch,
+  Param,
+  Query,
+  Delete,
+  NotFoundException,
+} from '@nestjs/common';
+
+import { CreateUserDto, UpdateUserDto } from './dto';
+import { UsersService } from './users.service';
 
 @Controller('api/v1/auth')
 export class UsersController {
+  constructor(private usersService: UsersService) {}
+
+  // @desc        : User signup.
+  // @Req. Data   : email, password.
+  // @route       : POST /api/v1/auth/signup.
+  // @access      : Public.
   @Post('/signup')
   createUser(@Body() body: CreateUserDto) {
-    console.log(body);
+    const { email, password } = body;
+    this.usersService.create(email, password);
+  }
+
+  // @desc        : Find user by id.
+  // @Req. Data   : id
+  // @route       : GET /api/v1/auth/:id
+  // @access      : Public
+  @Get('/:id')
+  async findUser(@Param('id') id: string) {
+    const user = await this.usersService.findById(parseInt(id));
+    if (!user) {
+      throw new NotFoundException('User not found');
+    }
+    return user;
+  }
+
+  // @desc        : Find user by email.
+  // @Req. Data   : email.
+  // @route       : GET /api/v1/auth?email=
+  // @access      : Public.
+  @Get()
+  findAllUsers(@Query('email') email: string) {
+    return this.usersService.findByEmail(email);
+  }
+
+  // @desc        : Delete user
+  // @Req. Data   : id
+  // @route       : DELETE /api/v1/auth/:id
+  // @access      : Public
+  @Delete('/:id')
+  removeUser(@Param('id') id: string) {
+    return this.usersService.remove(parseInt(id));
+  }
+
+  // @desc        : Update user details.
+  // @Req. Data   : id && opt: email || password
+  // @route       : DELETE /api/v1/auth/:id
+  // @access      : Public
+  @Patch('/:id')
+  updateUser(@Param('id') id: string, @Body() body: UpdateUserDto) {
+    return this.usersService.update(parseInt(id), body);
   }
 }
--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,4 +1,51 @@
-import { Injectable } from '@nestjs/common';
+/*
+ *
+ * Users Services.
+ *
+ */
+
+// Dependencies.
+import { Injectable, NotFoundException } from '@nestjs/common';
+import { Repository } from 'typeorm';
+import { InjectRepository } from '@nestjs/typeorm';
+
+import { User } from './user.entity';
 
 @Injectable()
-export class UsersService {}
+export class UsersService {
+  constructor(@InjectRepository(User) private repo: Repository<User>) {}
+
+  create(email: string, password: string) {
+    const user = this.repo.create({ email, password });
+
+    return this.repo.save(user);
+  }
+
+  findById(id: number) {
+    return this.repo.findOne({ where: { id: id } });
+  }
+
+  findByEmail(email: string) {
+    return this.repo.findOne({ where: { email: email } });
+  }
+
+  async update(id: number, attrs: Partial<User>) {
+    const user = await this.findById(id);
+    if (!user) {
+      throw new NotFoundException('user not found!');
+    }
+    Object.assign(user, attrs);
+
+    return this.repo.save(user);
+  }
+
+  async remove(id: number) {
+    const user = await this.findById(id);
+
+    if (!user) {
+      throw new NotFoundException('User not found!');
+    }
+
+    return this.repo.remove(user);
+  }
+}
