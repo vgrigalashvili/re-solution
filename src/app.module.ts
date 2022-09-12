0493983ade3bc83a895f441807b7c9be71757bb9
vgrigalashvili <lado.grigalashvili@gmail.com>
2022-09-12 19:25:35 +0400

    feat(mailer): adding mailing feature

--- src/app.module.ts
+++ src/app.module.ts
@@ -8,6 +8,9 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { ConfigModule, ConfigService } from '@nestjs/config';
+import { MailerModule } from '@nestjs-modules/mailer';
+import { PugAdapter } from '@nestjs-modules/mailer/dist/adapters/pug.adapter';
+
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
@@ -21,6 +24,7 @@ import { User } from './users/user.entity';
       isGlobal: true,
       envFilePath: `.env.${process.env.NODE_ENV}`,
     }),
+
     TypeOrmModule.forRootAsync({
       inject: [ConfigService],
       useFactory: (config: ConfigService) => {
@@ -31,6 +35,34 @@ import { User } from './users/user.entity';
           synchronize: true,
         };
       },
+    }),
+    MailerModule.forRootAsync({
+      inject: [ConfigService],
+      useFactory: (config: ConfigService) => ({
+        transport: {
+          host: config.get('MAILER_HOST'),
+          port: config.get('MAILER_PORT'),
+          tls: {
+            ciphers: config.get('MAILER_SEC'),
+          },
+          secure: false, // true for 465, false for other ports
+          auth: {
+            user: config.get('MAILER_USER'),
+            pass: config.get('MAILER_PASS'),
+          },
+        },
+        defaults: {
+          from: config.get('EMAIL_FROM'), // sender address
+          subject: 'Testing Nest MailerModule ✔', // Subject line
+        },
+        template: {
+          dir: __dirname + '/templates',
+          adapter: new PugAdapter(),
+          options: {
+            strict: true,
+          },
+        },
+      }),
     }),
     UsersModule,
     AuthModule,
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -9,6 +9,7 @@ import { BadRequestException, Injectable } from '@nestjs/common';
 import * as argon from 'argon2';
 import { JwtService } from '@nestjs/jwt';
 import { ConfigService } from '@nestjs/config';
+import { MailerService } from '@nestjs-modules/mailer';
 
 import { UsersService } from '../users/users.service';
 
@@ -18,6 +19,7 @@ export class AuthService {
     private usersService: UsersService,
     private config: ConfigService,
     private jwt: JwtService,
+    private readonly mailerService: MailerService,
   ) {}
 
   // @Service: User SignUp.
@@ -25,12 +27,27 @@ export class AuthService {
     // Check if email is in use.
     let user = await this.usersService.findByEmail(email);
     if (user) {
-      throw new BadRequestException('email already taken!');
+      throw new BadRequestException('Email already taken!');
     }
     // Hash the users password.
     const hash = await argon.hash(password);
     // Create a new user and save.
     user = await this.usersService.create(email, hash);
+
+    // Temporary, for testing purposes.
+    this.mailerService
+      .sendMail({
+        to: user.email, // list of receivers
+        text: 'Welcome to re:solution', // plaintext body
+        html: '<b>Welcome to re:solution</b>', // HTML body content
+      })
+      .then(() => {
+        console.log('Success');
+      })
+      .catch((ex) => {
+        console.log('Error:', ex);
+      });
+
     // Return the user.
     return user;
   }
@@ -39,13 +56,13 @@ export class AuthService {
   async signIn(email: string, password: string) {
     // Find the user by email.
     const user = await this.usersService.findByEmail(email);
-    // If user does not exist throw an exepton.
+    // If user does not exist throw an exception.
     if (!user) {
       throw new BadRequestException('Invalid credentials!');
     }
     // Compare passwords.
     const pwMatches = await argon.verify(user.password, password);
-    // If password incorrect trow exeption.
+    // If password incorrect trow exception.
     if (!pwMatches) {
       throw new BadRequestException('Invalid credentials!');
     }
